6cb9766d16ec03c7e56adeb3db1e7775348863ce
melinatabor <melina.tabor@gmail.com>
2024-04-14 20:50:41 -0300

    add: Mapper, BLL y BE Cliente

---
+++ BE/BECliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class BECliente
+    {
+    #region Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    #endregion
+
+    #region Constructores
+        public BECliente() { }
+
+        public BECliente(int id, string nombre, string apellido, string email)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Email = email;
+        }
+    #endregion
+
+    }
+}
