eba062c02138af5599c396168f03fd2acb50d35b
melinatabor <melina.tabor@gmail.com>
2024-04-18 20:27:38 -0300

    add: agrega una nueva propiedad del usuario

--- BE/BEUsuario.cs
+++ BE/BEUsuario.cs
@@ -8,6 +8,7 @@ namespace BE
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         #endregion
 
         #region Constructor
