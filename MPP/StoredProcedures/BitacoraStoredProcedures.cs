90e21e2bc4f2d46021688df41f40d21470b47799
Juan Ignacio Rios <juanrios0495@gmail.com>
2024-05-08 22:06:22 -0300

    Bitacora stored procedures

---
+++ MPP/StoredProcedures/BitacoraStoredProcedures.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MPP
+{
+    internal class BitacoraStoredProcedures
+    {
+        public const string SP_AgregarBitacora = "SP_Agregar_Bitacora";
+        public const string SP_FiltrarBitacora = "SP_Filtrar_Bitacora";
+    }
+}
