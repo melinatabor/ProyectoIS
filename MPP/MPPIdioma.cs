626fc25b529d1a9a5def6bd063a0f51c50732dc4
Juan Ignacio Rios <juanrios0495@gmail.com>
2024-06-07 21:13:19 -0300

    Refactor

--- MPP/MPPIdioma.cs
+++ MPP/MPPIdioma.cs
@@ -5,9 +5,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MPP
 {
